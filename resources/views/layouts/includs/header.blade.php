f1918f533a901db6907a2121b8f5a844a8081817
hiba77moh <hiba77moh@gmail.com>
2025-03-01 20:41:44 -0800

    removing login/logout from the dashboard

--- resources/views/layouts/includs/header.blade.php
+++ resources/views/layouts/includs/header.blade.php
@@ -44,16 +44,20 @@
         </div>
     </div>
     <div id="header-right" class="flex items-center md:space-x-6">
-        <div class="flex space-x-5">
-            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                href="http://127.0.0.1:8000/login">
-                Login
-            </a>
-            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                href="http://127.0.0.1:8000/register">
-                Register
-            </a>
-        </div>
+        @guest
+            <div class="flex space-x-5">
+                <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                    href="http://127.0.0.1:8000/login">
+                    Login
+                </a>
+                <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                    href="http://127.0.0.1:8000/register">
+                    Register
+                </a>
+            </div>
+        @endguest
+
+
         <div class="ms-3 relative">
             <x-dropdown align="right" width="48">
                 <x-slot name="trigger">
